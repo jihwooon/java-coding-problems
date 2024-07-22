40debd0e49be5919585cc17e79822bbabd31d044
jihwooon <jihwooon@gmail.com>
2024-07-22 22:56:02 +0900

    두 배열 합치기 문제 해결 방식 개선 및 테스트 수정
    - ArrayList 대신 int[] 배열을 사용하여 결과를 저장하도록 변경하여 메모리 효율성 향상
    - 불필요한 변수 제거 (left, right)
    - Arrays.asList() 대신 new int[]{}를 사용하여 테스트 코드 가독성 개선

--- src/test/java/org/example/두배열합치기.java
+++ src/test/java/org/example/두배열합치기.java
@@ -2,41 +2,40 @@ package org.example;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class 두배열합치기 {
 
-    private ArrayList<Integer> solution(int[] arr1, int[] arr2) {
-        ArrayList<Integer> answer = new ArrayList<>();
+    private int[] solution(int[] arr1, int[] arr2) {
+        int[] result = new int[arr1.length + arr2.length];
         int i = 0;
         int j = 0;
+        int k = 0;
         int left = arr1.length - 1;
         int right = arr2.length;
 
         while (i <= left && j <= right) {
             if (arr1[i] < arr2[j]) {
-                answer.add(arr1[i++]);
+                result[k++] = arr1[i++];
             } else {
-                answer.add(arr2[j++]);
+                result[k++] = arr2[j++];
             }
         }
 
         while (i < left) {
-            answer.add(arr1[i++]);
+            result[k++] = arr1[i++];
         }
 
         while (j < right) {
-            answer.add(arr2[j++]);
+            result[k++] = arr2[j++];
         }
 
-        return answer;
+        return result;
     }
 
     @Test
     public void result() {
         assertThat(solution(new int[]{1, 3, 5}, new int[]{2, 3, 6, 7, 9})).isEqualTo(
-            List.of(1, 2, 3, 3, 5, 6, 7, 9));
+            new int[]{1, 2, 3, 3, 5, 6, 7, 9});
     }
 }
